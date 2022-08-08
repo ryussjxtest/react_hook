ae4c490d835d1d0c479e41fc8b8d07c411acfd1f
ryussjxtest <ryussjxtest@gmail.com>
2022-08-08 14:52:52 +0900

    useState1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [time, setTime]=  useState(2);
+  const handleClick = () =>{
+    let newTime;
+    newTime = time == 24? 1: time+1 
+    setTime(newTime);
+  }
+
+  console.log("엡데이트!!");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <span> 현재 시각 : {time==24?0:time}시, {(time >= 12 && time<24)? "PM" : "AM"}{time>12? time-12:time}시</span>
+      <button onClick={handleClick}>Update</button>
     </div>
   );
 }
