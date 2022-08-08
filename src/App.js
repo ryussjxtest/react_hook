a4e55f8226c63594a103fe929955723d5ea7074d
ryussjxtest <ryussjxtest@gmail.com>
2022-08-08 18:09:16 +0900

    re

--- src/App.js
+++ src/App.js
@@ -7,10 +7,37 @@ import UseEffect from './hooktest/UseEffect';
 function App() {
  return(
     <div>
-      <UseEffect />
-      <div> <p> ================================ </p></div> 
+import './App.css';
+import { useState } from 'react';
+import UseState from './hooktest/UseState';
+import UseEffect from './hooktest/UseEffect';
+import Timer from './hooktest/Timer';
+import UseRef from './hooktest/UseRef';
+
+
+function App() {
+  const [showTimer, setShowTimer] =  useState(false);
+
+ return(
+    <div>
       
+      <div> <p> ======= [[  UseRef ]] ======= </p></div> 
+      <UseRef />
+      <div> <p> ======= [[  UseEffect  cleanup ]] ======= </p></div> 
+      {showTimer && <Timer />}
+      <button onClick={()=>{setShowTimer(!showTimer)}}>Toggle Timer..</button>
+
+      <div> <p> ======= [[  UseEffect ]] ======= </p></div> 
+      <UseEffect />
+
+      <div> <p> ======= [[  UseState ]] ======= </p></div>       
       <UseState />
+    </div>
+  );
+}
+
+export default App;
+
     </div>
   );
 }
